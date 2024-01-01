78cde11e64e789fc0a459fb7238169692f2cbd02
John D <johnd@Johns-MBP.attlocal.net>
2023-12-31 18:39:05 -0600

    psudo code

---
+++ script.js
@@ -0,0 +1,10 @@
+//first create the HTML
+//create the start button
+//link the button to the javascript
+//write the function to start the timer when the start button is clicked
+//write the function that decides to either progress after a question is answered completely 
+//or when answered incompletely deducts time from the clock.
+//the function should progress to game over status after all the questions are answered. 
+//game over should also be reached if time runs out on the clock.
+//write a new function that works after the game has ended.
+//the end function should be a promt where the user can enter their initials and their score is saved.
