a230a39e426f0b8b3ae881a0919a7817efc507be
John D <johnd@Johns-MBP.attlocal.net>
2024-01-02 22:45:38 -0600

    updated pseudo code

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 //first create the HTML
 //create the start button
-//when the start button is clicked a timer begins and questions start
 //link the button to the javascript
-//write the function to start the timer when the start button is clicked
+
+//when the start button is clicked a timer begins and questions start
+//link a funtion to start the timer in the timer div, that also removes the start div,
+//that also gets the questions into the question section.
+
 //write the function that decides to either progress after a question is answered completely 
 //or when answered incompletely deducts time from the clock.
+
 //the function should progress to game over status after all the questions are answered. 
 //game over should also be reached if time runs out on the clock.
+
 //write a new function that works after the game has ended.
 //the end function should be a promt where the user can enter their initials and their score is saved.
 
@@ -19,9 +24,9 @@ const Correct = "That's Right"
 const Incorrect = "Nope!"
 
 
-function start () {
-    alert("hello")
 
+function start (){
+    alert("hello")
 }
     
     
