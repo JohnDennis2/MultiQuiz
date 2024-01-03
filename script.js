0af775b07cbb3bbc77cac23f839e522909f4e3a0
John D <johnd@Johns-MBP.attlocal.net>
2024-01-02 19:36:21 -0600

    questions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //first create the HTML
 //create the start button
+//when the start button is clicked a timer begins and questions start
 //link the button to the javascript
 //write the function to start the timer when the start button is clicked
 //write the function that decides to either progress after a question is answered completely 
@@ -9,3 +10,47 @@
 //write a new function that works after the game has ended.
 //the end function should be a promt where the user can enter their initials and their score is saved.
 
+const 
+
+function startTimer() {
+    setTimeout(, 15000)
+}
+
+let questions = [
+    {
+        Number: 1
+        question: "What does a # mean in HTML?",
+        answer: "The element ID",
+        choices: [
+            "Its a number",
+            "The element ID",
+            "A telephone number",
+            "Its a deleted property",
+
+        ]
+    }
+        {
+            Number: 2
+            question: "What is a P tag?",
+            answer: "The paragraph element tag",
+            choices: [
+                "A spraypaint object",
+                "The property symbol",
+                "An adjective for party function",
+                "A function named Property",
+    
+            ]
+    }
+    {
+        Number: 3
+        question: "What is javascript?",
+        answer: "A programming language",
+        choices: [
+            "A coffe bar",
+            "A programming language",
+            "A program to write novels",
+            "A form of martial arts",
+
+        ]
+    }
+]
